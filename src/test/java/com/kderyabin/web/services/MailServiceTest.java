92e377eb633765edc83bf202ad82365e77e63473
Konstantin Deryabin <kderyabin@orange.fr>
2020-08-25 10:53:14 +0200

    mail service ut

--- src/test/java/com/kderyabin/web/services/MailServiceTest.java
+++ src/test/java/com/kderyabin/web/services/MailServiceTest.java
@@ -1,21 +1,28 @@
 package com.kderyabin.web.services;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 
+import javax.mail.Address;
+import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class MailServiceTest {
+
+	final private Logger LOG = LoggerFactory.getLogger(MailServiceTest.class);
 
 	@Autowired
 	MailService mailService;
@@ -42,8 +49,23 @@ class MailServiceTest {
 
 
     @Test
-    void isSendMail() {
-		// mail sending is disabled for tests
-		assertFalse(mailService.isSendMail());
+    void sendConfirmationMail() throws MessagingException, IOException {
+		String sender = "sender@somewhere.com";
+		String recipientEmail = "kderyabin@somewhere.com";
+		String recipientName = "Konstantin Deryabin";
+		String link = "http://localhost:8080/fr/xxxxxxxxxxxxxxxxxxx";
+		Locale locale = Locale.ENGLISH;
+		mailService.setLocale(locale);
+		mailService.setSendMail(false);
+		mailService.setSenderAddress(sender);
+		mailService.sendConfirmationMail(recipientEmail, recipientName, link);
+		MimeMessage message = mailService.getMimeMessage();
+
+		Address address = message.getRecipients(Message.RecipientType.TO)[0];
+		LOG.debug(address.toString());
+		assertEquals(recipientEmail, address.toString());
+
+		Address from = message.getFrom()[0];
+		assertEquals(sender, from.toString());
     }
 }
